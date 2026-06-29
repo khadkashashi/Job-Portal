44300cf06b0fad23691b7328bda682cb9d984403
shashi khadka <shashikhadka84@gmail.com>
2026-06-29 13:27:54 +0545

    company module

--- companies/urls.py
+++ companies/urls.py
@@ -3,9 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(
-        "create/",
-        views.create_company,
-        name="create-company",
-    ),
+    path("create/", views.create_company, name="create-company"),
+    path("profile/", views.company_profile, name="company-profile"),
 ]
