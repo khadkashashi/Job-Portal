86df312f16354491a963ec672adcb06de1d43209
shashi khadka <shashikhadka84@gmail.com>
2026-07-10 23:30:07 +0545

    fix: browser jobs

--- landingpage/views.py
+++ landingpage/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from jobs.models import Job
 from companies.models import Company
 
@@ -14,3 +15,20 @@ def home(request):
     return render(request,"landingpage/home.html", context)
 def about(request):
     return render(request, "landingpage/about.html")
+
+
+class PublicJobListView(ListView):
+    model = Job
+    template_name = "jobs/public_job_list.html"
+    context_object_name = "jobs"
+    paginate_by = 10
+
+    def get_queryset(self):
+        queryset = Job.objects.filter(is_active=True)
+        keyword = self.request.GET.get("keyword")
+        location = self.request.GET.get("location")
+        if keyword:
+            queryset = queryset.filter(title__icontains=keyword)
+        if location:
+            queryset = queryset.filter(location__icontains=location)
+        return queryset.order_by("-created_at")
